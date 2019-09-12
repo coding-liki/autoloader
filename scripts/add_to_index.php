f1b92b6c2b6e2d2339adcc129f435bc82d6e6da5
lev <coding1liki@gmail.com>
2019-09-13 02:36:00 +0300

    Исправил работу скрипта

--- scripts/add_to_index.php
+++ scripts/add_to_index.php
@@ -11,10 +11,13 @@ if(!file_exists($path)){
     file_put_contents($path, "<?php\n$str");
 } else {
     $index_contents = file_get_contents($path);
-    $index_parts = explode("<?php", $index_contents);
-    array_unshift($index_parts, $str);
-    $index_contents = implode("<?php", $index_parts);
-
+    $index_parts = explode('<?php', $index_contents);
+    // array_shift($index_parts);
+    // array_unshift($index_parts, $str);
+    
+    $index_parts[1] = $str.$index_parts[1];
+    $index_contents = implode('<?php', $index_parts);
+    var_dump($index_parts);
     file_put_contents($path, $index_contents);
 }
 
