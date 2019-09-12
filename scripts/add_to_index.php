02955c238f5253a7c9e54a940124bbf100fd44fe
lev <coding1liki@gmail.com>
2019-09-13 01:20:22 +0300

    Добавил скрипт

---
+++ scripts/add_to_index.php
@@ -0,0 +1,21 @@
+<?php
+
+$path = __DIR__."../../../../index.php";
+
+$str = <<<PHP
+
+require_once "vendors/CodingLiki/Autoloader/autoloader.php";
+
+PHP;
+if(!file_exists($path)){
+    file_put_contents($path, "<?php\n$str");
+} else {
+    $index_contents = file_get_contents($path);
+    $index_parts = explode("<?php", $index_contents);
+    array_unshift($index_parts, $str);
+    $index_contents = implode("<?php", $index_parts);
+
+    file_put_contents($path, $index_contents);
+}
+
+
